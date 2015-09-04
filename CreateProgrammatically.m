32c2d91df5503abce972b95139cb9ada7fd164fa
iosdevvivek <iosdev.vivek@gmail.com>
2015-09-04 19:07:18 +0530

    table view Delegates.
    table view Delegates.

--- CreateProgrammatically.m
+++ CreateProgrammatically.m
@@ -12,7 +12,45 @@
     
     [self.view addSubview:btn];
     
+//UiLabel...
 
+  // Uilabel
+    
+    UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(30, 300, 140, 40)];
+    lbl.text = @"test";
+    lbl.textAlignment = NSTextAlignmentCenter;
+    lbl.backgroundColor = [UIColor grayColor];
+    [lbl setFontHeleveticaNueueBold:17];
+    
+    [self.view addSubview:lbl];
+    
+    //Table View Delegate Methods
+    - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [your array count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    static NSString *CellIdentifier = @"Cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    if (cell == nil) {
+
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+
+    }
+
+    // Configure the cell...
+    cell.textLabel.text = [yourarray objectAtIndex:indexPath.row];
+
+    return cell;
+
+}
 
 
 
