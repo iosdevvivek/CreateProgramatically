6cb6ca72af4510da2a2f5725677923828eef3a44
iosdevvivek <iosdev.vivek@gmail.com>
2015-09-07 15:04:40 +0530

    Create Toast.m

---
+++ Toast.m
@@ -0,0 +1,17 @@
+-(void)showtestWithMessage:(NSString*)msg
+{
+    NSString *message = msg;
+    
+    UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:nil, nil];
+    [toast show];
+    
+    int duration = 1; // duration in seconds
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        [toast dismissWithClickedButtonIndex:0 animated:YES];
+    });
+}
